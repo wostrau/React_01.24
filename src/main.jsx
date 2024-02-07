d8361a0a4c36b32f5c07ab3c6a37dab3035f359a
wostrau <a.ostrovtsov@gmail.com>
2024-02-08 00:50:58 +0100

    added ResultScreen component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { App } from './App.jsx'
+import { QuizSettingsProvider } from './context/QuizSettingsContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QuizSettingsProvider>
+      <App />
+    </QuizSettingsProvider>
+  </React.StrictMode>
 )
