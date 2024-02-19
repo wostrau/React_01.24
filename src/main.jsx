c187b0e0b6c66a76d821772b518437842d1acf9d
wostrau <a.ostrovtsov@gmail.com>
2024-02-19 22:27:11 +0100

    minor updates in main.jsx to clean up the rest of context providers and imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { AnswersProvider } from './context/AnswersContext.jsx'
 import { StoreProvider } from './store/StoreProvider.jsx'
 import { BasicRouter } from './navigation/BasicRouter.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-      <AnswersProvider>
-        <StoreProvider>
-          <BasicRouter />
-        </StoreProvider>
-      </AnswersProvider>
+  <StoreProvider>
+    <BasicRouter />
+  </StoreProvider>
   //</React.StrictMode>
 )
