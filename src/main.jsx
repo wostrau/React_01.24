f36d32dd78581348bdde3d9954198b021fe7d459
wostrau <a.ostrovtsov@gmail.com>
2024-02-22 15:04:36 +0100

    added redux-persist library for saving statistics slice ONLY

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import { RouterProvider } from 'react-router-dom'
 
 import './index.css'
 import { App } from './App.jsx'
-import { store } from './store/store.js'
+import { persistor, store } from './store/store.js'
 import { router } from './navigation/router.jsx'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <RouterProvider router={router}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </RouterProvider>
   </Provider>
   // </React.StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+import * as rp from 'redux-persist'
 
 import { triviaApi } from './triviaApi'
 import { quizReducer } from './quizReducer'
 import { settingsReducer } from './settingsReducer'
 import { statisticsReducer } from './statisticsReducer'
 
-const combinedReducers = {
+const reducers = combineReducers({
   quiz: quizReducer,
   settings: settingsReducer,
   statistics: statisticsReducer,
   [triviaApi.reducerPath]: triviaApi.reducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['statistics']
 }
 
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 export const store = configureStore({
-  reducer: combinedReducers,
-  middleware: (gDM) => gDM().concat(triviaApi.middleware)
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [rp.FLUSH, rp.REHYDRATE, rp.PAUSE, rp.PERSIST, rp.PURGE, rp.REGISTER]
+      }
+    }).concat(triviaApi.middleware)
 })
+
+export const persistor = persistStore(store)
